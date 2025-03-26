225744d8d8aba932812b7d46a941bf9bab1788bb
dscao <dscao.com@gmail.com>
2025-03-27 02:21:34 +0800

    重新设计ip分组等添加删除逻辑
    重新设计ip分组、ipv6分组添加删除逻辑：
    先获取新列表，成功后删除旧分组，再添加新分组。
    弃用delOldRule参数的作用。

--- runModeUpdateIpgroup.go
+++ runModeUpdateIpgroup.go
@@ -11,120 +11,62 @@ func updateIpgroup() {
 		return
 	}
 
-	if *delOldRule == "before" {
-		log.Println("Tips: 在添加之前会强制删除所有备注包含 IKUAI_BYPASS 字符的ip分组和端口分流")
-		err = iKuai.DelIKuaiBypassIpGroup("cleanAll")
-		if err != nil {
-			log.Println("ip分组== 删除旧的IP分组失败,退出：", err)
-			return
-		} else {
-			log.Println("ip分组== 删除旧的IP分组成功")
-		}
-	}
-
 	for _, ipGroup := range conf.IpGroup {
-		if *delOldRule == "after" {
-			preIds, err := iKuai.GetIpGroup(ipGroup.Name)
-			if err != nil {
-				log.Println("ip分组== 获取准备更新的IP分组列表失败：", ipGroup.Name, err)
-				continue
-			} else {
-				log.Println("ip分组== 获取准备更新的IP分组列表成功", ipGroup.Name, preIds)
-			}
 
-			err = updateIpGroup(iKuai, ipGroup.Name, ipGroup.URL)
-			if err != nil {
-				log.Printf("ip分组== 添加IP分组'%s@%s'失败：%s\n", ipGroup.Name, ipGroup.URL, err)
-			} else {
-				log.Printf("ip分组== 添加IP分组'%s@%s'成功\n", ipGroup.Name, ipGroup.URL)
-				err = iKuai.DelIpGroup(preIds)
-				if err == nil {
-					log.Println("ip分组== 删除旧的IP分组列表成功", ipGroup.Name)
-					log.Println("ip分组== 更新完成", ipGroup.Name)
-				} else {
-					log.Println("ip分组== 删除旧的IP分组列表有错误", ipGroup.Name, err)
-				}
-			}
-		} else {
-			err := updateIpGroup(iKuai, ipGroup.Name, ipGroup.URL)
-			if err != nil {
-				log.Printf("ip分组== 添加IP分组'%s@%s'失败：%s\n", ipGroup.Name, ipGroup.URL, err)
-			} else {
-				log.Printf("ip分组== 添加IP分组'%s@%s'成功\n", ipGroup.Name, ipGroup.URL)
-			}
-		}
-	}
-
-	if *delOldRule == "before" {
-		err = iKuai.DelIKuaiBypassStreamIpPort("cleanAll")
+		preIds, err := iKuai.GetIpGroup(ipGroup.Name)
 		if err != nil {
-			log.Println("端口分流== 删除旧的端口分流失败,退出：", err)
-			return
+			log.Println("ip分组== 获取准备更新的IP分组列表失败：", ipGroup.Name, err)
+			continue
 		} else {
-			log.Println("端口分流== 删除旧的端口分流成功")
+			log.Println("ip分组== 获取准备更新的IP分组列表成功", ipGroup.Name, preIds)
 		}
-	}
-	for _, streamIpPort := range conf.StreamIpPort {
-		if *delOldRule == "after" {
-			preIds, err := iKuai.GetStreamIpPortIds(streamIpPort.IpGroup)
-			if err != nil {
-				log.Println("端口分流== 获取准备更新的端口分流列表失败：", streamIpPort.IpGroup, err)
-				continue
-			} else {
-				log.Println("端口分流== 获取准备更新的端口分流列表成功", streamIpPort.IpGroup, preIds)
-			}
 
-			err = updateStreamIpPort(
-				iKuai,
-				streamIpPort.Type,
-				streamIpPort.Interface,
-				streamIpPort.Nexthop,
-				streamIpPort.SrcAddr,
-				streamIpPort.IpGroup,
-				streamIpPort.Mode,
-				streamIpPort.IfaceBand,
-			)
-			if err != nil {
-				log.Printf("端口分流== 添加端口分流 '%s@%s' 失败：%s\n",
-					streamIpPort.Interface+streamIpPort.Nexthop,
-					streamIpPort.IpGroup,
-					err,
-				)
-			} else {
-				log.Printf("端口分流== 添加端口分流 '%s@%s' 成功\n",
-					streamIpPort.Interface+streamIpPort.Nexthop,
-					streamIpPort.IpGroup,
-				)
-				err = iKuai.DelStreamIpPort(preIds)
-				if err == nil {
-					log.Println("端口分流== 删除旧的端口分流列表成功", streamIpPort.IpGroup, preIds)
-					log.Println("端口分流== 更新完成", streamIpPort.IpGroup)
-				} else {
-					log.Println("端口分流== 删除旧的端口分流列表有错误", streamIpPort.IpGroup, err)
-				}
-			}
+		err = updateIpGroup(iKuai, ipGroup.Name, ipGroup.URL, preIds)
+		if err != nil {
+			log.Printf("ip分组== 添加IP分组'%s@%s'失败：%s\n", ipGroup.Name, ipGroup.URL, err)
 		} else {
-			err := updateStreamIpPort(
-				iKuai,
-				streamIpPort.Type,
-				streamIpPort.Interface,
-				streamIpPort.Nexthop,
-				streamIpPort.SrcAddr,
+			log.Printf("ip分组== 添加IP分组'%s@%s'成功\n", ipGroup.Name, ipGroup.URL)
+		}
+
+	}
+
+	for _, streamIpPort := range conf.StreamIpPort {
+		preIds, err := iKuai.GetStreamIpPortIds(streamIpPort.IpGroup)
+		if err != nil {
+			log.Println("端口分流== 获取准备更新的端口分流列表失败：", streamIpPort.IpGroup, err)
+			continue
+		} else {
+			log.Println("端口分流== 获取准备更新的端口分流列表成功", streamIpPort.IpGroup, preIds)
+		}
+
+		err = updateStreamIpPort(
+			iKuai,
+			preIds,
+			streamIpPort.Type,
+			streamIpPort.Interface,
+			streamIpPort.Nexthop,
+			streamIpPort.SrcAddr,
+			streamIpPort.IpGroup,
+			streamIpPort.Mode,
+			streamIpPort.IfaceBand,
+		)
+		if err != nil {
+			log.Printf("端口分流== 添加端口分流 '%s@%s' 失败：%s\n",
+				streamIpPort.Interface+streamIpPort.Nexthop,
 				streamIpPort.IpGroup,
-				streamIpPort.Mode,
-				streamIpPort.IfaceBand,
+				err,
 			)
-			if err != nil {
-				log.Printf("端口分流== 添加端口分流 '%s@%s' 失败：%s\n",
-					streamIpPort.Interface+streamIpPort.Nexthop,
-					streamIpPort.IpGroup,
-					err,
-				)
+		} else {
+			log.Printf("端口分流== 添加端口分流 '%s@%s' 成功\n",
+				streamIpPort.Interface+streamIpPort.Nexthop,
+				streamIpPort.IpGroup,
+			)
+			err = iKuai.DelStreamIpPort(preIds)
+			if err == nil {
+				log.Println("端口分流== 删除旧的端口分流列表成功", streamIpPort.IpGroup, preIds)
+				log.Println("端口分流== 更新完成", streamIpPort.IpGroup)
 			} else {
-				log.Printf("端口分流== 添加端口分流 '%s@%s' 成功\n",
-					streamIpPort.Interface+streamIpPort.Nexthop,
-					streamIpPort.IpGroup,
-				)
+				log.Println("端口分流== 删除旧的端口分流列表有错误", streamIpPort.IpGroup, err)
 			}
 		}
 	}
@@ -136,46 +78,21 @@ func updateIpv6group() {
 		log.Println("登录爱快失败：", err)
 		return
 	}
-	if *delOldRule == "before" {
-		log.Println("Tips: 在添加之前会强制删除所有备注包含 IKUAI_BYPASS 字符的ipv6分组")
-		err = iKuai.DelIKuaiBypassIpv6Group("cleanAll")
-		if err != nil {
-			log.Println("ipv6分组== 删除旧的IPV6分组失败,退出：", err)
-			return
-		} else {
-			log.Println("ipv6分组== 删除旧的IPV6分组成功")
-		}
-	}
+	
 	for _, ipv6Group := range conf.Ipv6Group {
-		if *delOldRule == "after" {
-			preIds, err := iKuai.GetIpv6Group(ipv6Group.Name)
-			if err != nil {
-				log.Println("ipv6分组== 获取准备更新的IPv6分组列表失败：", ipv6Group.Name, err)
-				continue
-			} else {
-				log.Println("ipv6分组== 获取准备更新的IPv6分组列表成功", ipv6Group.Name, preIds)
-			}
-
-			err = updateIpv6Group(iKuai, ipv6Group.Name, ipv6Group.URL)
-			if err != nil {
-				log.Printf("ipv6分组== 添加IPV6分组'%s@%s'失败：%s\n", ipv6Group.Name, ipv6Group.URL, err)
-			} else {
-				log.Printf("ipv6分组== 添加IPV6分组'%s@%s'成功\n", ipv6Group.Name, ipv6Group.URL)
-				err = iKuai.DelIpv6Group(preIds)
-				if err == nil {
-					log.Println("ipv6分组== 删除旧的IPv6分组列表成功", ipv6Group.Name, preIds)
-					log.Println("ipv6分组== 更新完成", ipv6Group.Name)
-				} else {
-					log.Println("ipv6分组== 删除旧的IPv6分组列表有错误", ipv6Group.Name, err)
-				}
-			}
+		preIds, err := iKuai.GetIpv6Group(ipv6Group.Name)
+		if err != nil {
+			log.Println("ipv6分组== 获取准备更新的IPv6分组列表失败：", ipv6Group.Name, err)
+			continue
 		} else {
-			err := updateIpv6Group(iKuai, ipv6Group.Name, ipv6Group.URL)
-			if err != nil {
-				log.Printf("ipv6分组== 添加IPV6分组'%s@%s'失败：%s\n", ipv6Group.Name, ipv6Group.URL, err)
-			} else {
-				log.Printf("ipv6分组== 添加IPV6分组'%s@%s'成功\n", ipv6Group.Name, ipv6Group.URL)
-			}
+			log.Println("ipv6分组== 获取准备更新的IPv6分组列表成功", ipv6Group.Name, preIds)
+		}
+
+		err = updateIpv6Group(iKuai, ipv6Group.Name, ipv6Group.URL, preIds)
+		if err != nil {
+			log.Printf("ipv6分组== 添加IPV6分组'%s@%s'失败：%s\n", ipv6Group.Name, ipv6Group.URL, err)
+		} else {
+			log.Printf("ipv6分组== 添加IPV6分组'%s@%s'成功\n", ipv6Group.Name, ipv6Group.URL)
 		}
 	}
 }
