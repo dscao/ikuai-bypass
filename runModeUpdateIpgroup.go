d35cc32d62b099e15f354c75ccbe1acba8b78139
dscao <dscao.com@gmail.com>
2025-03-27 11:10:17 +0800

    优化分流规则的增加删除逻辑

--- runModeUpdateIpgroup.go
+++ runModeUpdateIpgroup.go
@@ -61,13 +61,6 @@ func updateIpgroup() {
 				streamIpPort.Interface+streamIpPort.Nexthop,
 				streamIpPort.IpGroup,
 			)
-			err = iKuai.DelStreamIpPort(preIds)
-			if err == nil {
-				log.Println("端口分流== 删除旧的端口分流列表成功", streamIpPort.IpGroup, preIds)
-				log.Println("端口分流== 更新完成", streamIpPort.IpGroup)
-			} else {
-				log.Println("端口分流== 删除旧的端口分流列表有错误", streamIpPort.IpGroup, err)
-			}
 		}
 	}
 }
